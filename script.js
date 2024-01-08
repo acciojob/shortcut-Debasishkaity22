abecf8ae0b5bc4fe610d8fcdd9d801786bf1ea27
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2024-01-08 19:19:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function shortcut(s1, s2) {
   // your code here
+	if(s1===""||s2==="") return "";
+	else{
+		let s="";
+		s+=s1.charAt(0)+s2.charAt(0);
+		return s;
+	}
 }
 
 // Do not change the code below.
